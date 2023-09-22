eb916e1700294c2357af6d8d4993ef31c2799d0e
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-09-22 11:30:50 +0200

    Delete and Edit functionality for Review

--- src/components/EditReview.jsx
+++ src/components/EditReview.jsx
@@ -1,9 +1,94 @@
-function EditReview() {
-    return (
-      <div>
-        <h1>Edit Review component</h1>
-      </div>
-    );
-  }
-   
-  export default EditReview;
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+ 
+ 
+function EditReview(props) {
+  console.log("MY HUMPS", props);
+  const [title, setTitle] = useState("");
+  const [review, setReview] = useState("");
+  const [rating, setRating] = useState("");
+  const [played, setPlayed] = useState("");
+
+  const navigate = useNavigate();
+  const gameId = useParams();
+  const reviewId = props._id
+
+  useEffect(() => {
+    axios
+    .get(`${import.meta.env.VITE_API_URL}/games/${gameId}/reviews/${props._id}`)
+      .then((response) => {
+        console.log("AHHH", response);
+        const reviewToEdit = response.data;
+        setTitle(reviewToEdit.title);
+        setReview(reviewToEdit.review);
+        setRating(reviewToEdit.rating);
+        setPlayed(reviewToEdit.played);
+      })
+      .catch((error) => console.log(error));
+    
+  }, [reviewId]);
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const requestBody = { title, review, rating, played };
+    axios
+    .put(`${import.meta.env.VITE_API_URL}/games/${props.game}/reviews/${reviewId}`, requestBody)
+      .then((response) => {
+        props.changeFunction()
+        navigate(`/games/${props.game}`)
+      })
+      .catch((err) => console.log(err));
+  };
+  
+
+
+  return (
+    <div>
+      <h3>Edit Review</h3>
+
+      <form onSubmit={handleFormSubmit}>
+        <label>Title:</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Rating:</label>
+        <input
+          type="number"
+          name="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <label>Review:</label>
+        <textarea
+          type="text"
+          name="review"
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+        />
+
+        <label>Played:</label>
+
+        <input
+          type="checkbox"
+          name="played"
+          checked={played}
+          onChange={(e) => setPlayed(e.target.checked)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+ 
+export default EditReview;
