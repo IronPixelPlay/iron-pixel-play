0b6c2842f944f04165d72cba4763e2e97981fc5d
Johanna Geßner <johanna-gessner@web.de>
2023-09-24 18:07:01 +0200

    implement css for game details

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import AddReview from "../components/AddReview";
+import { Card } from 'react-bootstrap';
 import ReviewList from "../components/ReviewList";
 
 function GameDetailsPage() {
@@ -32,19 +31,27 @@ function GameDetailsPage() {
 
     return (
       <div>
-        <h1>{game.title}</h1>
-        <img src={game.image} alt={game.title} />
-        <p>Category: {game.category}</p>
-        <p>Description:{game.description}</p>
-        <p>Instructions: {game.instructions}</p>
-        <p>Demo: <Link>{game.demo}</Link></p>
-        <p>GitHub-Link: <Link>{game.gitHubLink}</Link></p>
-        
-        <ReviewList/>
+        <Card bg="dark" text="white" className="card-with-spacing bright-shadow">
+          <Card.Body className="text-center">
+            <Card.Title><h1>{game.title}</h1></Card.Title>
+            <Card.Img style={{ maxWidth: '100%', maxHeight: '200px', objectFit: 'contain' }} variant="top" src={game.image} alt={game.title} />
+            <Card.Text><strong>Category:</strong> <p>{game.category}</p></Card.Text>
+            <Card.Text><strong>Description:</strong> <p>{game.description}</p></Card.Text>
+            <Card.Text><strong>Instructions:</strong> <p>{game.instructions}</p></Card.Text>
+            <Card.Text>
+            <strong>Demo:</strong> <p><a href={game.demo}>Play Demo</a></p>
+            </Card.Text>
+            <Card.Text>
+              <strong>GitHub-Link:</strong><p><a href={game.gitHubLink}>GitHub</a></p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <ReviewList />
       </div>
     );
   }
   return <>{renderList()}</>;
 }
 
-  export default GameDetailsPage;
+export default GameDetailsPage;
