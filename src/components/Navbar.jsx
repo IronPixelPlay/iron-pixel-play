81521647a307e226d5d309ce0c1a332c19b9a77f
Johanna Geßner <johanna-gessner@web.de>
2023-09-25 13:12:37 +0200

    make the navbar responsive with hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,51 +11,77 @@ function NavBar() {
   } = useContext(AuthContext);
 
   return (
-    <Navbar className="bright-shadow" bg="dark" expand="lg" style={{ borderRadius: '10px' }}>
-      <Container>
-        <Navbar.Brand as={NavLink} to="/" style={{ color: 'white' }}>
-          Home
-        </Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link as={NavLink} to="/games" style={{ color: 'white' }}>
-            Games
-          </Nav.Link>
-          {isLoggedIn && (
-            <>
-              <Nav.Link as={NavLink} to="/games/create" style={{ color: 'white' }}>
-                Upload your Game
+    <Navbar sticky="top" className="bright-shadow" bg="dark" expand="lg" style={{ borderRadius: '10px' }}>
+  <Container>
+    <Navbar.Brand as={NavLink} to="/" style={{ color: 'white' }}>
+      Home
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mx-auto"> {/* Use mx-auto class to center align the Nav */}
+        <Nav.Link as={NavLink} to="/games" style={{ color: 'white' }}>
+          Games
+        </Nav.Link>
+        {isLoggedIn && (
+          <>
+            <Nav.Link as={NavLink} to="/games/create" style={{ color: 'white' }}>
+              Upload your Game
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/user" style={{ color: 'white' }}>
+              My Profile
+            </Nav.Link>
+          </>
+        )}
+      </Nav>
+      <div className="navbar-right">
+        {isLoggedIn ? (
+          <>
+            <span className="navbar-text d-lg-none" style={{ color: 'white' }}>
+              Hi {user && user.name}!
+            </span>
+            <Button variant="danger" onClick={logOutUser} className="d-lg-none">
+              Logout
+            </Button>
+          </>
+        ) : (
+          <>
+            <Nav>
+              <Nav.Link as={NavLink} to="/signup" style={{ color: 'white' }} className="d-lg-none">
+                Sign Up
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/user" style={{ color: 'white' }}>
-                My Profile
+              <Nav.Link as={NavLink} to="/login" style={{ color: 'white' }} className="d-lg-none">
+                Login
               </Nav.Link>
-            </>
-          )}
-        </Nav>
-        <div className="navbar-right">
-          {isLoggedIn ? (
-            <>
-              <span className="navbar-text" style={{ color: 'white' }}>
-                Hi {user && user.name}!
-              </span>
-              <Button variant="danger" onClick={logOutUser}>
-                Logout
-              </Button>
-            </>
-          ) : (
-            <>
-              <Nav>
-                <Nav.Link as={NavLink} to="/signup" style={{ color: 'white' }}>
-                  Sign Up
-                </Nav.Link>
-                <Nav.Link as={NavLink} to="/login" style={{ color: 'white' }}>
-                  Login
-                </Nav.Link>
-              </Nav>
-            </>
-          )}
-        </div>
-      </Container>
-    </Navbar>
+            </Nav>
+          </>
+        )}
+      </div>
+    </Navbar.Collapse>
+    <div className="navbar-right d-none d-lg-block">
+      {isLoggedIn ? (
+        <>
+          <span className="navbar-text" style={{ color: 'white' }}>
+            Hi {user && user.name}!
+          </span>
+          <Button variant="danger" onClick={logOutUser}>
+            Logout
+          </Button>
+        </>
+      ) : (
+        <>
+          <Nav>
+            <Nav.Link as={NavLink} to="/signup" style={{ color: 'white' }}>
+              Sign Up
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/login" style={{ color: 'white' }}>
+              Login
+            </Nav.Link>
+          </Nav>
+        </>
+      )}
+    </div>
+  </Container>
+</Navbar>
   );
 }
 
