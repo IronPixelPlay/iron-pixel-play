daa6e28f166b54a5d971fae1ea2cf123ae8e0241
Johanna Geßner <johanna-gessner@web.de>
2023-09-28 12:19:22 +0200

    css changes on game cards

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -8,20 +8,21 @@ function GameCard({ title, description, image, _id }) {
 
     return (
         <Card bg="dark" text="white" className="card-with-spacing bright-shadow">
-            <Card.Img style={{ width: '100%', height: '170px', objectFit: 'cover' }} variant="top" src={imageUrl} />
-            <Card.Body>
-                <Card.Title>
-                    {title}
-                </Card.Title>
-                <Card.Text>
-                    {description}
-                </Card.Text>
-                <div className="d-flex justify-content-center">
-                    <Link to={`/games/${_id}`} className="btn btn-primary">Visit the Game</Link>
-                </div>
-            </Card.Body>
+          <Card.Img style={{ width: '100%', height: '190px', objectFit: 'cover' }} variant="top" src={imageUrl} />
+          <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <Card.Title>
+              {title}
+            </Card.Title>
+            <Card.Text>
+              {description}
+            </Card.Text>
+            <div className="d-flex justify-content-center">
+              <Link to={`/games/${_id}`} className="btn btn-primary">Visit the Game</Link>
+            </div>
+          </Card.Body>
         </Card>
-    );
+      );
+      
 }
 
 export default GameCard;
